9ec034a0b16611e6333a8b2bb46171d42399592b
CocoNut <vladik.ef@mail.ru>
2025-02-28 00:06:35 +0300

    Cosmetical

---
+++ src/get_data.py
@@ -0,0 +1,10 @@
+# import pandas as pd
+
+
+# class Data:
+#   @staticmethod
+#   def get_features_train():
+#     return(pd.read_csv("./data/train.csv", index_col=0).iloc[:, 0:5])
+  
+#   @staticmethod
+#   def get_test
