78449d060ab222e5ca328341be8fca0e9802dc99
CocoNut <vladik.ef@mail.ru>
2025-05-21 21:49:51 +0300

    random forest

--- src/metrics.py
+++ src/metrics.py
@@ -1,4 +1,6 @@
 from sklearn.metrics import accuracy_score, recall_score, f1_score, confusion_matrix, precision_score
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def metrics(X, Y, to_return=True):  # X predicted, Y real
@@ -10,7 +12,10 @@ def metrics(X, Y, to_return=True):  # X predicted, Y real
     print("Precision: ", scores["Precision"])
     print("Recall:    ", scores["Recall"])
     print("F1:        ", scores["F1"])
-    print(confusion_matrix(Y, X))
+    ax = sns.heatmap(confusion_matrix(Y, X), annot=True, vmax=X.size, vmin=-X.size,
+                     fmt="d", cmap="crest")
+    ax.set(xlabel="Predicted class", ylabel="Real class")
+    plt.show()
 
     if to_return:
       return scores
