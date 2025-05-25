d7c88bf479de5634c79b48778c1a1378d1fe4eae
Arnautov <j32arnautov@gmail.com>
2025-05-25 16:29:37 +0300

    Added gradbust.

--- src/metrics.py
+++ src/metrics.py
@@ -3,19 +3,22 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def metrics(X, Y, to_return=True):  # X predicted, Y real
+def metrics(pred, real, to_return=True, output=True):  # X predicted, Y real
+    X = pred
+    Y = real
     scores = {"Accuracy": accuracy_score(Y, X),
-              "Precision": precision_score(Y, X),
-              "Recall": recall_score(Y, X),
-              "F1": f1_score(Y, X)}
-    print("Accuracy:  ", scores["Accuracy"])
-    print("Precision: ", scores["Precision"])
-    print("Recall:    ", scores["Recall"])
-    print("F1:        ", scores["F1"])
-    ax = sns.heatmap(confusion_matrix(Y, X), annot=True, vmax=X.size, vmin=0,
-                     fmt="d", cmap="Blues")
-    ax.set(xlabel="Predicted class", ylabel="Real class")
-    plt.show()
+              "Precision": precision_score(Y, X, zero_division=0),
+              "Recall": recall_score(Y, X, zero_division=0),
+              "F1": f1_score(Y, X, zero_division=0)}
+    if output:
+        print("Accuracy:  ", scores["Accuracy"])
+        print("Precision: ", scores["Precision"])
+        print("Recall:    ", scores["Recall"])
+        print("F1:        ", scores["F1"])
+        ax = sns.heatmap(confusion_matrix(Y, X), annot=True, vmax=X.size, vmin=0,
+                         fmt="d", cmap="Blues")
+        ax.set(xlabel="Predicted class", ylabel="Real class")
+        plt.show()
 
     if to_return:
         return scores
