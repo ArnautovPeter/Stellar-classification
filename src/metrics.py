0c75b43f0efdfff1204d87508350de69ad3aaf9a
Arnautov <j32arnautov@gmail.com>
2025-05-29 21:29:47 +0300

    Added ability to change cmap color in metrics.

--- src/metrics.py
+++ src/metrics.py
@@ -1,20 +1,22 @@
 from sklearn.metrics import accuracy_score, recall_score, f1_score, confusion_matrix, precision_score, classification_report
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import numpy as np
 
 
-def metrics(pred, real, to_return=True, output=True):  # X predicted, Y real
+def metrics(pred, real, to_return=True, output=True, cmap="Blues", vmin=0):  # X predicted, Y real
     X = pred
     Y = real
     if output:
-        print(classification_report(X, Y))
-        ax = sns.heatmap(confusion_matrix(Y, X), annot=True, vmax=X.size, vmin=0,
-                         fmt="d", cmap="Blues")
+        print(classification_report(X, Y, zero_division=0))
+        ax = sns.heatmap(confusion_matrix(Y, X), annot=True, vmax=X.size, vmin=vmin,
+                         fmt="d", cmap=cmap)
         ax.set(xlabel="Predicted class", ylabel="Real class")
         plt.show()
 
     if to_return:
         return {"Accuracy": accuracy_score(Y, X),
-              "Precision": precision_score(Y, X, zero_division=0),
-              "Recall": recall_score(Y, X, zero_division=0),
-              "F1": f1_score(Y, X, zero_division=0)}
+                "Precision": precision_score(Y, X, zero_division=0),
+                "Recall": recall_score(Y, X, zero_division=0),
+                "F1": f1_score(Y, X, zero_division=0)}
